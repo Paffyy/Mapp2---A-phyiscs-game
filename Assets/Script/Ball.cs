4a14c7b21f1ba2edf3b8f17719709d944105acb3
taey1 <37865938+taey1@users.noreply.github.com>
2018-04-12 14:08:01 +0200

    test

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public Rigidbody2D rgdb;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
