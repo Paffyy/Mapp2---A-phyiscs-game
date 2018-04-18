cc052b1beeddbb4ef47d217d95fcacfcb8591e8b
taey1 <37865938+taey1@users.noreply.github.com>
2018-04-18 02:28:44 +0200

    asd

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour {
 
     public Ball ball;
-    public Button btnStart;
+    public Button playButton;
+    public Button stopButton;
+    public Button resetButton;
+    public Button homeButton;
     public GameObject startPanel;
     private Vector3 startLocation;
     public CircleCollider2D endArea;
@@ -16,6 +20,8 @@ public class GameController : MonoBehaviour {
         startLocation = ball.transform.position;
         ball.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
         tapped = new RaycastHit2D();
+        stopButton.interactable = false;
+        stopButton.image.enabled = false;
     }
 
     // Update is called once per frame
@@ -39,7 +45,7 @@ public class GameController : MonoBehaviour {
                 
             }
         }
-        if (isColliding())
+        if (IsColliding())
         {
             ball.transform.position = startLocation;
             ball.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
@@ -47,7 +53,7 @@ public class GameController : MonoBehaviour {
             startPanel.SetActive(true);
         }
     }
-    public bool isColliding()
+    public bool IsColliding()
     {
         return ball.GetComponent<CircleCollider2D>().IsTouching(endArea);
     }
@@ -55,7 +61,22 @@ public class GameController : MonoBehaviour {
     {
         startPanel.SetActive(false);
         ball.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.None;
+        playButton.interactable = false;
+        playButton.image.enabled = false;
+        stopButton.interactable = true;
+        stopButton.image.enabled = true;
     }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ResetBoard()
+    {
+        SceneManager.LoadScene(1);
+    }
+
     //Vector3 getScreenPoint()
     //{
     //    return Camera.main.WorldToScreenPoint(new Vector3(Input.touches[0].deltaPosition.x, Input.touches[0].deltaPosition.y  screenPoint.z));
