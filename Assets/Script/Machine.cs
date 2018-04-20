e816c5b114d5cbf67001fe5b01d35035fb30964c
Patric Wåhlin <pw@teamwahlin.se>
2018-04-20 15:46:57 +0200

    Machine Template
    Fixat en template för tillägning av maskiner.

--- Assets/Script/Machine.cs
+++ Assets/Script/Machine.cs
@@ -1,17 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Machine : MonoBehaviour {
-    public Sprite Sprite;
-    public Transform Location;
     public GameObject settingsPanel;
+    public Button ButtonTemplate;
+    public string[] actionNames;
+    public Button.ButtonClickedEvent[] actions;
+    private int flip;
+    private RaycastHit2D tapped;
     private Collider2D colliderClick;
     void Start()
-    {§
+    {
         HideSettingsPanel();
-        Location = GetComponent<Transform>();
+        for (int i = 0; i < actions.Length || i < actionNames.Length; i++)
+        {
+            AddButtonToPanel(actionNames[i], actions[i], i );
+        }
         colliderClick = GetComponent<Collider2D>();
+        tapped = new RaycastHit2D();
+    }
+    private void Update()
+    {
+        foreach (var item in Input.touches)
+        {
+            if (item.phase == TouchPhase.Began)
+            {
+                ShowSettingsPanel();
+            }
+        }
     }
     public void ShowSettingsPanel()
     {
@@ -24,6 +43,34 @@ public class Machine : MonoBehaviour {
     }
     public Vector3 GetLocation()
     {
-        return new Vector3(Location.position.x + 1.8f, Location.position.y + 1.5f, 0);
+        return new Vector3(transform.position.x + 1.8f * 150, transform.position.y + 1.5f* 250, transform.position.z);
+    }
+    public void AddButtonToPanel(string buttonText, Button.ButtonClickedEvent actions, int i)
+    {
+        Button button = Instantiate(ButtonTemplate, new Vector3(0,110- 60*i), new Quaternion(0f,0f,0f,0f));
+        var text = button.GetComponentInChildren<Text>();
+        text.text = buttonText;
+        button.onClick = actions;
+        button.transform.SetParent(settingsPanel.transform, false);
+    }
+    public void OnMouseDown()
+    {
+
+        ShowSettingsPanel();
+    }
+
+
+    public void Flip()
+    {
+        flip = flip == 1 ? -1 : 1;
+        transform.localScale = new Vector3(transform.transform.localScale.x * flip, transform.transform.localScale.y, transform.transform.localScale.z);
+    }
+    public void FlipLeft()
+    {
+        transform.localScale = new Vector3(-1, transform.transform.localScale.y, transform.transform.localScale.z);
+    }
+    public void FlipRight()
+    {
+        transform.localScale = new Vector3(1, transform.transform.localScale.y, transform.transform.localScale.z);
     }
 }
