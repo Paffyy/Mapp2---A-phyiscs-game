62da6e824d08cc7ed8732f6b12ceec8b4ded7041
Paffyy <pw@teamwahlin.se>
2018-04-22 18:12:25 +0200

    Timer script
    (Move to GameController) l8r

---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+    public float timeToFinish;
+    public bool isLevelTimed;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartLevelTimer(isLevelTimed);
+	}
+    public void StartLevelTimer(bool isLevelTimed)
+    {
+        if(isLevelTimed)
+        {
+            timeToFinish -= Time.deltaTime;
+            if (timeToFinish < 0)
+            {
+                Debug.Log("end");
+            }
+        }
+    }
+}
