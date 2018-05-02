05414abd1d969d369a69fa9f01a90bc546f48887
taey1 <37865938+taey1@users.noreply.github.com>
2018-05-02 14:02:06 +0200

    animation gubbe

---
+++ Assets/Script/AnimationController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour {
+
+    public GameObject professorRight;
+    public GameObject professorLeft;
+    public BoxCollider2D leftCollider;
+    public BoxCollider2D rightCollider;
+    private bool professor;
+
+	// Use this for initialization
+	void Start () {
+        professorLeft.SetActive(false);
+        professor = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (rightCollider.IsTouching(professorRight.GetComponent<BoxCollider2D>())){
+            Debug.Log("hej");
+            professorLeft.SetActive(true);
+            professorRight.SetActive(false);
+       
+        }
+
+        if (leftCollider.IsTouching(professorLeft.GetComponent<BoxCollider2D>())){
+            Debug.Log("hej2");
+            professorRight.SetActive(true);
+            professorLeft.SetActive(false);
+        }
+		
+	}
+}
