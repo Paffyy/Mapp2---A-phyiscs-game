f23f8505a2207abf96dfc0746ded5cab9a8120f5
tovajaderberg <tova.t.jaderberg@gmail.com>
2018-05-13 22:23:05 +0200

    Rocket

--- Assets/Script/Rocket.cs
+++ Assets/Script/Rocket.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
 	public Rigidbody2D ball;
-	public Rigidbody2D rocket;
+    public Collider2D rocketCollider;
+    public Collider2D rocketDoor;
+	private Rigidbody2D rocket;
+    //public SpriteRenderer ramp;
+    public Vector2 rocketForce = new Vector2(0.0f, 0.5f);
+    public float FlyTime;
 
 	// Use this for initialization
 	void Start () {
 		rocket = GetComponent<Rigidbody2D>();
-
 	}
 
 	// Update is called once per frame
@@ -18,13 +22,28 @@ public class Rocket : MonoBehaviour {
 
 	}
 
-/*
 	private void OnTriggerStay2D(Collider2D coll){
-			if (coll.CompareTag("ball")){
-				ball.transform.parent = this.transform;
-				rocket.AddForce( ForceMode.Impulse);
+			if (coll.CompareTag("Ball")){
+            //ramp.enabled = false;
+            Invoke("FlyRocket", 1);
+            Invoke("FreeBall", FlyTime);
 			}
 	}
 
-	*/
+    private void FlyRocket()
+    {
+        ball.transform.parent = rocket.transform;
+        rocketDoor.enabled = true;
+        rocket.constraints = RigidbodyConstraints2D.None;
+        rocket.AddForce(rocketForce, ForceMode2D.Impulse);
+    }
+
+    private void FreeBall()
+    {
+        rocketDoor.enabled = false;
+        rocketCollider.enabled = false;
+        ball.transform.parent = null;
+    }
+
+
 }
