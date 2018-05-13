72afcb5e9b223063de0de2845c8e8152a6b14518
tovajaderberg <tova.t.jaderberg@gmail.com>
2018-05-13 12:12:42 +0200

    Rocket
    I egen game scene, script, mm

---
+++ Assets/Script/Rocket.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+	public Rigidbody2D ball;
+	public Rigidbody2D rocket;
+
+	// Use this for initialization
+	void Start () {
+		rocket = GetComponent<Rigidbody2D>();
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+/*
+	private void OnTriggerStay2D(Collider2D coll){
+			if (coll.CompareTag("ball")){
+				ball.transform.parent = this.transform;
+				rocket.AddForce( ForceMode.Impulse);
+			}
+	}
+
+	*/
+}
