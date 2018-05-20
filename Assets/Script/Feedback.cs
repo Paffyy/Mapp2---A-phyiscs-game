08edd5d694f0d8f64b74a8351ba5c0db62f3e03c
Patric Wåhlin <pw@teamwahlin.se>
2018-05-20 23:56:08 +0200

    PlayerFeedback

---
+++ Assets/Script/Feedback.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Feedback : MonoBehaviour {
+
+    public GameObject NextFeedback;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.touchCount > 0)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                GoToNextFeedback();
+            }
+        }
+        else if (Input.anyKeyDown)
+        {
+            GoToNextFeedback();
+        }
+	}
+
+    public void GoToNextFeedback()
+    {
+        if (NextFeedback == null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            NextFeedback.SetActive(true);
+            Destroy(this.gameObject);
+        }
+    }
+}
