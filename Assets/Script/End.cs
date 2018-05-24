60c44c04bedca75865af79f59c3a00c608674e11
Bazzan <sebake01@gmail.com>
2018-05-24 22:09:56 +0200

    Heal vfx

--- Assets/Script/End.cs
+++ Assets/Script/End.cs
@@ -7,18 +7,22 @@ public class End : MonoBehaviour {
     public GameObject ball;
     public GameObject deadRat;
     public GameObject aliveRat;
+    public ParticleSystem VfxHeal;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update () {
 		if (ball.GetComponent<CircleCollider2D>().IsTouching(this.GetComponent<CircleCollider2D>()))
         {
             deadRat.SetActive(false);
             aliveRat.SetActive(true);
+            VfxHeal.Play();
+
+
         }
 	}
 }
